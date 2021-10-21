24a72667b4056282b1ad5095b82c22b9caa05ef0
fathur-k <fathurkaryim@gmail.com>
2021-10-21 22:06:42 +0700

    account controller done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const port = 3000
 const router = require('./routers/index')
 const app = express()
 const multer = require('multer')
+const session = require("express-session");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require('cloudinary').v2
 const storage = new CloudinaryStorage({
@@ -15,6 +16,15 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({ storage });
 
+app.use(
+  session({
+    secret: "judge-irvann",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,15 +2,53 @@ const { Account, Buyer, Seller } = require('../models/index')
 
 class AccountController {
   static login (req, res) {
-    
+    res.render('login')
   }
 
   static postLogin (req, res) {
+    Account.findOne({where: {email: req.body.email}})
+    .then( data => {
+      if(data.password == req.body.password) {
+        req.session.email = data.email
+        req.session.role = data.role
 
+        if(data.role == 'seller') {
+          res.redirect('/sellers')
+        } else {
+          res.redirect('/buyers')
+        }
+      } else {
+        throw new Error('Passwords do not match')
+      }
+    })
+    .catch( err => {
+      res.send(err)
+    })
   }
 
   static register (req, res) {
-    
+    let { username, email, password, phonenumber, role } = req.body
+    Account.findOne({where: {email}})
+    .then( data => {
+      if(data) {
+        throw new Error('Email has been registered')
+      } else {
+        return Account.create({ username, email, password, phonenumber, role })
+      }
+    })
+    .then( data => {
+      req.session.email = email
+      req.session.role = role
+
+      if(role == 'seller') {
+        res.redirect('/sellers')
+      } else {
+        res.redirect('/buyers')
+      }
+    })
+    .catch( err => {
+      res.send(err)
+    })
   }
 
   static postRegister (req, res) {
